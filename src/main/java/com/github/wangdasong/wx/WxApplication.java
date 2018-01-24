10375550f599965f030ac4943118474ad89755c8
wangdasong <wangds@cpscs.cc>
2018-01-24 15:24:51 +0800

    tomcat适配

--- src/main/java/com/github/wangdasong/wx/WxApplication.java
+++ src/main/java/com/github/wangdasong/wx/WxApplication.java
@@ -3,13 +3,19 @@ package com.github.wangdasong.wx;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
 @MapperScan("com.github.wangdasong.wx.dao.persistence")
-public class WxApplication {
-
+public class WxApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder application) {
+		return application.sources(WxApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WxApplication.class, args);
 	}
