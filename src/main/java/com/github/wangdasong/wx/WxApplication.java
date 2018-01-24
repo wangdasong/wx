724b84dc7e222a8137de4197ff0884dc3cb7c7f8
wangdasong <wangds@cpscs.cc>
2018-01-24 14:04:15 +0800

    修改工程名称

--- src/main/java/com/github/wangdasong/wx/WxApplication.java
+++ src/main/java/com/github/wangdasong/wx/WxApplication.java
@@ -1,9 +1,13 @@
 package com.github.wangdasong.wx;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
+@MapperScan("com.github.wangdasong.wx.dao.persistence")
 public class WxApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/github/wangdasong/wx/controller/BonusController.java
+++ src/main/java/com/github/wangdasong/wx/controller/BonusController.java
@@ -1,15 +1,19 @@
 package com.github.wangdasong.wx.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController(value = "/api/bonus")
+@Controller
+@RequestMapping(value = "/api/bonus")
 public class BonusController {
-    @RequestMapping(value = "getBonus2LeftCount")
+    @RequestMapping(value = "/getBonus2LeftCount")
+    @ResponseBody
     public String getBonus2LeftCount(){
         return "100";
     }
-    @RequestMapping(value = "getBonus3LeftCount")
+    @RequestMapping(value = "/getBonus3LeftCount")
+    @ResponseBody
     public String getBonus3LeftCount(){
         return "200";
     }
