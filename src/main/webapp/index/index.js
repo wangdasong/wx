4c78d42058e744156f409d31719bb34eb58670b0
wangdasong <wangds@cpscs.cc>
2018-01-29 16:35:59 +0800

    增加公众号功能

--- src/main/webapp/index/index.js
+++ src/main/webapp/index/index.js
@@ -20,6 +20,7 @@ var renderUserInfo = function(){
 }
 (function () {
     var authCode = getQueryVariable(code);
+    alert("authCode ====" + authCode);
     if(!authCode){
         window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx731f37de4b367a23&redirect_uri=https://www.wangdasong.top/index/index.html&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect";
     }
